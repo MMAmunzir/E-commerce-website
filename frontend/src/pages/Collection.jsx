1b810aa1cff3d7d8a5dd3bc9ec043f9c2fa3fbe8
MMAmunzir <munzirmuneer1998@gmail.com>
2025-04-05 10:04:32 +0530

    frontend home page created

---
+++ frontend/src/pages/Collection.jsx
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+
+const Collection = () => {
+  const { products } = useContext(ShopContext);
+
+  return <div>Collection</div>;
+};
+
+export default Collection;
