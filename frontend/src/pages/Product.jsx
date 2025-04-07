34ecaffe5bce5b36bb97dbe48ab1ed17aba3b232
MMAmunzir <munzirmuneer1998@gmail.com>
2025-04-07 14:05:31 +0530

    created cart page relatedproducts

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,7 +1,136 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { useParams } from "react-router-dom";
+import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
-  return <div>Product</div>;
+  const { productId } = useParams();
+  const { products, currency, addToCart } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item);
+        setImage(item.image[0]);
+        return null;
+      }
+    });
+  };
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
+
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      {/* Product Data */}
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+        {/* Product image */}
+        <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between  sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((item, index) => (
+              <img
+                onClick={() => {
+                  setImage(item);
+                }}
+                src={item}
+                key={index}
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+              />
+            ))}
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img src={image} className="w-full h-auto" />
+          </div>
+        </div>
+        {/* product info */}
+        <div className="flex-1">
+          <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
+          <div className="flex items-center gap-1 mt-2">
+            <img src={assets.star_icon} className="w-3 5" />
+            <img src={assets.star_icon} className="w-3 5" />
+            <img src={assets.star_icon} className="w-3 5" />
+            <img src={assets.star_icon} className="w-3 5" />
+            <img src={assets.star_dull_icon} className="w-3 5" />
+            <p className="pl-2">(123)</p>
+          </div>
+          <p className="mt-5 text-3xl font-medium">
+            {currency}
+            {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`border py-2 px-4 bg-gray-100 ${
+                    item === size ? "border-orange-500" : ""
+                  }`}
+                  key={index}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button
+            onClick={() => addToCart(productData._id, size)}
+            className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"
+          >
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1"></div>
+          <p className="text-gray-500">100% Original product</p>
+          <p className="text-gray-500">
+            Cash on delivery is available on this product
+          </p>
+          <p className="text-gray-500">
+            Easy return and exchange policy withing 7 days
+          </p>
+        </div>
+        {/* Description & Review section */}
+      </div>
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews(123)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            Experience unmatched comfort and style with our premium product,
+            crafted with precision and designed to elevate your everyday life.
+            Made from high-quality materials, this product ensures durability
+            and long-lasting performance. Whether you're at work, at home, or on
+            the go, its sleek design and versatile functionality make it the
+            perfect companion for any occasion.
+          </p>
+          <p>
+            With attention to detail and a focus on user satisfaction, this
+            product offers a seamless blend of practicality and elegance. Its
+            innovative features are tailored to meet your needs, while the
+            modern aesthetic adds a touch of sophistication to your lifestyle.
+            Choose this product for a reliable, stylish, and efficient solution
+            that stands out from the rest.
+          </p>
+        </div>
+      </div>
+      {/* display related products */}
+      <RelatedProducts
+        category={productData.category}
+        subCategory={productData.subCategory}
+      />
+    </div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
 };
 
 export default Product;
