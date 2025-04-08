6b26e5756d8489fe8f7c0d2a4c53369930f45a91
MMAmunzir <munzirmuneer1998@gmail.com>
2025-04-08 20:01:56 +0530

    added cart page placeOrder

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,9 +2,11 @@ import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
+import CartTotal from "./CartTotal";
+import { Navigate } from "react-router-dom";
 
 const Cart = () => {
-  const { currency, products, cartItems, updateQuantity } =
+  const { currency, products, cartItems, updateQuantity, navigate } =
     useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
@@ -79,6 +81,20 @@ const Cart = () => {
             </div>
           );
         })}
+      </div>
+
+      <div className="flex justify-end my-20">
+        <div className="w-full sm:w-[450px]">
+          <CartTotal />
+          <div className="w-full text-end">
+            <button
+              onClick={() => navigate("/place-order")}
+              className="bg-black text-white text-sm my-8 px-8 py-3 cursor-pointer"
+            >
+              PROCEED TO CHECKOUT
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
