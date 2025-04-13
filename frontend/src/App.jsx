a72757ec7d3b063d9f9c0b3921f6d407d95fd022
MMAmunzir <munzirmuneer1998@gmail.com>
2025-04-13 20:43:49 +0530

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ import Footer from "./components/Footer.jsx";
 import SearchBar from "./components/SearchBar.jsx";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import VerifyPayment from "./pages/verifyPayment.jsx";
+import VerifyPayment from "./pages/VerifyPayment.jsx";
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
